b70e0b5abb8edb47638bab6bbfbe82055c3bf332
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-05-26 22:31:32 -0600

    optimize code

--- script.js
+++ script.js
@@ -33,15 +33,10 @@ $(document).ready(function () {
   $(this).siblings(".description").val(localStorage.getItem($(this).parent().attr("id")));
 
   // refresh and still there
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  for(i=9; i<17 ;i++){
+    $('#hour-'+i+' .description').val(localStorage.getItem('hour-'+i));
+  }
+  
   // refresh the time every 30s to compair again
   newHour();
   setInterval(newHour, 30000);
